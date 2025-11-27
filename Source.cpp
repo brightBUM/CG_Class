a5868628b7c39955e2b7a5739cccf6a86020dea2
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2025-11-27 12:49:29 +0530

    Quad via EBO

--- Source.cpp
+++ Source.cpp
@@ -18,6 +18,9 @@ void FPSCounter(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+//global variables
+float modelZ = 0.0f;
+
 int main()
 {
     // glfw: initialize and configure
@@ -60,20 +63,29 @@ int main()
     // ------------------------------------------------------------------
     float vertices[] = {
         // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+        -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   // top left - 0
+         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right - 1
+         0.5f,  -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom left - 2
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f  // bottom right -3
     };
 
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -81,11 +93,12 @@ int main()
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    // glBindVertexArray(0);
+    glBindVertexArray(0);
 
-
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -105,7 +118,7 @@ int main()
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 proj = glm::mat4(1.0f);
 
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 5.0f));
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, modelZ));
         //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5, 0.5f, 0.5f));
 
@@ -117,14 +130,15 @@ int main()
 
         ourShader.SetMat4("model", model);
         ourShader.SetMat4("view", view);
-        ourShader.SetMat4("proj", view);
+        ourShader.SetMat4("proj", proj);
         //ourShader.setFloat("uTime", glfwGetTime());
 
 
         // render the triangle
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -169,6 +183,10 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        modelZ -= 0.1f;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        modelZ += 0.1f;
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
