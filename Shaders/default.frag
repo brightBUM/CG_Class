d89608f16b1b467ab3bd7c8a19cba38e1a6a545d
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2025-11-28 12:17:39 +0530

    Texture part 2

--- Shaders/default.frag
+++ Shaders/default.frag
@@ -1,8 +1,12 @@
 #version 330 core
 out vec4 FragColor;
+
 in vec3 Color;
+in vec2 TexCoord;
+uniform sampler2D texSampler;
 void main()
 {
-
-   FragColor = vec4(Color, 1.0f);
+   FragColor = texture(texSampler,TexCoord);
+    
+   //FragColor = vec4(Color, 1.0f);
 }
--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -102,6 +101,35 @@ int main()
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    stbi_set_flip_vertically_on_load(true);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("Textures/punchCat.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    
+
+    stbi_image_free(data);
+
+    ourShader.use();
+    ourShader.setInt("texture", 0);
+
+
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     // render loop
     // -----------
@@ -140,6 +168,7 @@ int main()
 
         // render the triangle
         ourShader.use();
+        glBindTexture(GL_TEXTURE, texture);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         //glBindVertexArray(0);
