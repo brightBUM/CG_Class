95435380a497cf7ef83bfa9dc34aaf3b8de66b78
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2025-12-02 11:43:59 +0530

    Cube pos and Color

--- Source.cpp
+++ Source.cpp
@@ -75,6 +75,43 @@ int main()
     0, 1, 3,   // first triangle
     1, 2, 3    // second triangle
     };
+    float cubeVertices[] = {
+        // position           // color
+        -0.5f,-0.5f,-0.5f,    1.0f,0.0f,0.0f,   // 0
+         0.5f,-0.5f,-0.5f,    0.0f,1.0f,0.0f,   // 1
+         0.5f, 0.5f,-0.5f,    0.0f,0.0f,1.0f,   // 2
+        -0.5f, 0.5f,-0.5f,    1.0f,1.0f,0.0f,   // 3
+
+        -0.5f,-0.5f, 0.5f,    1.0f,0.0f,1.0f,   // 4
+         0.5f,-0.5f, 0.5f,    0.0f,1.0f,1.0f,   // 5
+         0.5f, 0.5f, 0.5f,    1.0f,1.0f,1.0f,   // 6
+        -0.5f, 0.5f, 0.5f,    0.3f,0.3f,0.3f    // 7
+    };
+    unsigned int cubeIndices[] = {
+        // Front face
+        0, 1, 2,
+        2, 3, 0,
+
+        // Right face
+        1, 5, 6,
+        6, 2, 1,
+
+        // Back face
+        5, 4, 7,
+        7, 6, 5,
+
+        // Left face
+        4, 0, 3,
+        3, 7, 4,
+
+        // Top face
+        3, 2, 6,
+        6, 7, 3,
+
+        // Bottom face
+        4, 5, 1,
+        1, 0, 4
+    };
 
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -114,7 +151,7 @@ int main()
     ourShader.setInt("texSampler2", 1);
 
 
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
